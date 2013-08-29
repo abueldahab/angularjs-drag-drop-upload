e2fae0db02a0b30a96161ece56f0bfc2718e1729
CloudDueling.com <michael@clouddueling.com>
2013-08-29 12:04:59 -0700

    Update directive.js

--- directive.js
+++ directive.js
@@ -1,117 +1,126 @@
-directives.directive('ngFileUpload', function() {
-  // Helper function that formats the file sizes
-  function formatFileSize(bytes) {
-    if (typeof bytes !== 'number') {
-      return '';
-    }
+directives.directive('dragDropUpload', function() {
+	// Helper function that formats the file sizes
+	function formatFileSize(bytes) {
+		if (typeof bytes !== 'number') {
+			return '';
+		}
 
-    if (bytes >= 1000000000) {
-      return (bytes / 1000000000).toFixed(2) + ' GB';
-    }
+		if (bytes >= 1000000000) {
+			return (bytes / 1000000000).toFixed(2) + ' GB';
+		}
 
-    if (bytes >= 1000000) {
-      return (bytes / 1000000).toFixed(2) + ' MB';
-    }
+		if (bytes >= 1000000) {
+			return (bytes / 1000000).toFixed(2) + ' MB';
+		}
 
-    return (bytes / 1000).toFixed(2) + ' KB';
-  }
+		return (bytes / 1000).toFixed(2) + ' KB';
+	}
 
-  return {
-    restrict: 'A',
-    link: function(scope, elem, attr, ctrl) {
-      var dragForm = "<form id='file-upload' method='post' action='" + attr.ngFileUpload + "' enctype='multipart/form-data'> \
-        <div id='drop'> \
-          Drop Here<br> \
-          <a>Browse</a> \
-          <input type='file' name='file' multiple /> \
-        </div> \
-        <ul></ul> \
-      </form>";
+	return {
+		restrict: 'A',
+		scope: {
+			action: '=',
+			onComplete: '&',
+			onProgress: '&',
+			onError: '&',
+		},
+		link: function(scope, elem, attr, ctrl) {
+			var dragForm = "<form id='file-upload' method='post' action='" + scope.action + "' enctype='multipart/form-data'> \
+				<div id='drop'> \
+					Drop Here<br> \
+					<a>Browse</a> \
+					<input type='file' name='file' multiple /> \
+				</div> \
+				<ul></ul> \
+			</form>";
 
-      elem.html(dragForm);
+			elem.html(dragForm);
 
-      var ul = $('#file-upload ul');
+			var ul = $('#file-upload ul');
 
-      $(document).on('click', '#drop a', function(){
-        // Simulate a click on the file input button
-        // to show the file browser dialog
-        $(this).parent().find('input').click();
-      });
+			$(document).on('click', '#drop a', function(){
+				// Simulate a click on the file input button
+				// to show the file browser dialog
+				$(this).parent().find('input').click();
+			});
 
-      // Initialize the jQuery File Upload plugin
-      $('#file-upload').fileupload({
-        // This element will accept file drag/drop uploading
-        dropZone: $('#drop'),
+			// Initialize the jQuery File Upload plugin
+			$('#file-upload').fileupload({
+				// This element will accept file drag/drop uploading
+				dropZone: $('#drop'),
 
-        // This function is called when a file is added to the queue;
-        // either via the browse button, or via drag/drop:
-        add: function (e, data) {
-          var tpl = $('<li class="working"><input type="text" value="0" data-width="48" data-height="48"'+
-            ' data-fgColor="#0788a5" data-readOnly="1" data-bgColor="#3e4043" /><p></p><span></span></li>');
+				// This function is called when a file is added to the queue;
+				// either via the browse button, or via drag/drop:
+				add: function (e, data) {
+					var tpl = $('<li class="working"><input type="text" value="0" data-width="48" data-height="48"'+
+						' data-fgColor="#0788a5" data-readOnly="1" data-bgColor="#3e4043" /><p></p><span></span></li>');
 
-          // Append the file name and file size
-          tpl.find('p').text(data.files[0].name)
-            .append('<i>' + formatFileSize(data.files[0].size) + '</i>');
+					// Append the file name and file size
+					tpl.find('p').text(data.files[0].name)
+						.append('<i>' + formatFileSize(data.files[0].size) + '</i>');
 
-          // Add the HTML to the UL element
-          data.context = tpl.appendTo(ul);
+					// Add the HTML to the UL element
+					data.context = tpl.appendTo(ul);
 
-          // Initialize the knob plugin
-          tpl.find('input').knob();
+					// Initialize the knob plugin
+					tpl.find('input').knob();
 
-          // Listen for clicks on the cancel icon
-          tpl.find('span').click(function(){
-            if(tpl.hasClass('working')){
-              jqXHR.abort();
-            }
+					// Listen for clicks on the cancel icon
+					tpl.find('span').click(function(){
+						if(tpl.hasClass('working')){
+							jqXHR.abort();
+						}
 
-            tpl.fadeOut(function(){
-              tpl.remove();
-            });
-          });
+						tpl.fadeOut(function(){
+							tpl.remove();
+						});
+					});
 
-          // Automatically upload the file once it is added to the queue
-          var jqXHR = data.submit();
-        },
+					// Automatically upload the file once it is added to the queue
+					var jqXHR = data.submit();
+				},
 
-        done: function(e, data) {
-          scope.$apply(function(s) {
-            s.$eval(attr.complete);
-          });
-        },
+				done: function(e, data) {
+					scope.$apply(function(s) {
+						scope.onComplete({ e: e, data: data });
+					});
+				},
 
-        progress: function(e, data){
-          // Calculate the completion percentage of the upload
-          var progress = parseInt(data.loaded / data.total * 100, 10);
+				progress: function(e, data){
+					// data loaded is the amount uploaded
+					var progress = parseInt(data.loaded / data.total * 100, 10);
 
-          // Update the hidden input field and trigger a change
-          // so that the jQuery knob plugin knows to update the dial
-          data.context.find('input').val(progress).change();
+					// Update the hidden input field and trigger a change
+					// so that the jQuery knob plugin knows to update the dial
+					data.context.find('input').val(progress).change();
 
-          if(progress == 100){
-            data.context.removeClass('working');
-          }
-        },
+					if(progress == 100)
+						data.context.removeClass('working');
 
-        fail:function(e, data){
-          scope.$apply(function(s) {
-            s.$eval(attr.error);
-          });
+					scope.$apply(function(s) {
+						scope.onProgress({ progress: progress, e: e, data: data })
+					});
+				},
 
-          data.context.addClass('error');
-        }
-      });
+				fail:function(e, data){
+					scope.$apply(function(s) {
+						scope.onError({ e: e, data: data })
+					});
 
-      // Prevent the default action when a file is dropped on the window
-      $(document).on('dragover', function (e) {
-        e.preventDefault();
-        $('#drop').addClass('active');
-      });
+					data.context.addClass('error');
+				}
+			});
 
-      $(document).on('drop dragleave', function (e) {
-        e.preventDefault();
-        $('#drop').removeClass('active');
-      });
-    }
-  }
+			// Prevent the default action when a file is dropped on the window
+			$(document).on('dragover', function (e) {
+				e.preventDefault();
+				$('#drop').addClass('active');
+			});
+
+			$(document).on('drop dragleave', function (e) {
+				e.preventDefault();
+				$('#drop').removeClass('active');
+			});
+		}
+	}
 });
